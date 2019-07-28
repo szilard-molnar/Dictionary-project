2cffda7e497d4102665403045c359575966ff370
Miguel Wolmers <rakasmurfs@gmail.com>
2019-07-28 16:13:32 -0400

    Added webster api search for max of 3 results

--- miguel/assets/javascript/app.js
+++ miguel/assets/javascript/app.js
@@ -5,26 +5,41 @@ $(document).ready(function()
     $("#submit").click(function(){
         var myExpression = $("#inputTerm").val();
         urbanTerm.push(myExpression);
-        console.log(urbanTerm);
         $("#inputTerm").val("");
 
-        var queryURL = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term="+urbanTerm+"";
+        //elementary dictionary
+        //var queryURL = "https://www.dictionaryapi.com/api/v3/references/sd2/json/" + myExpression + "?key=8a432f3f-03bf-4acd-9217-bd6489771a25";
 
+        //collegiate dictionary
+        var queryURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + myExpression + "?key=8a432f3f-03bf-4acd-9217-bd6489771a25";
+
+    
     $.ajax({
         url: queryURL, 
-        headers: {"X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com",
-        "X-RapidAPI-Key": "748ddc26b8mshb590d128ea3a157p115cd4jsn1816f6826646"},
         method: "GET"
     
     }).then(function (response)
     {
-        var results = response.list;
-        console.log(results);
-
-        for(var i = 0; i < results.length; i++)
-        {
-            $("#urbanPlace").html(results[i].definition);
+        results = response[0].shortdef
+        console.log(response);
+        $("#websterDiv").html("");
+        var pos = 1;
+        if (results)
+            {
+                $("#websterDiv").append("<p>" + myExpression + "</p>");
+                for (var i = 0; i < results.length; i++)
+                    {
+                        if(i <=2)
+                            {
+                                $("#websterDiv").append("<p>" + pos + ": " + results[i] + "</p>")
+                                pos++
+                            }
+                    }                
+            }
+        else{
+            $("#websterDiv").append("That doth not be a word. . . bro.")
         }
+
     })
 })
 })
