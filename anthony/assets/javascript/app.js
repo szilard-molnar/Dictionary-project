b1ba541dbbf045e4b8fd6b4cd5442f4ecd3d30a8
Anthony <anthony.irizarry1025@gmail.com>
2019-07-29 20:35:30 -0400

    finalize gif

--- anthony/assets/javascript/app.js
+++ anthony/assets/javascript/app.js
@@ -1,30 +1,23 @@
 $(document).ready(function()
 {   
-    var urbanTerm = [];
+    var searchWord = [];
 
-    $("#submit").click(function(){
+    $("#submit").click(function(callGif){
         var myExpression = $("#inputTerm").val();
-        urbanTerm.push(myExpression);
-        console.log(urbanTerm);
+        searchWord.push(myExpression);
+        console.log(searchWord);
         $("#inputTerm").val("");
-
-        var queryURL = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term="+urbanTerm+"";
-
-    $.ajax({
-        url: queryURL, 
-        headers: {"X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com",
-        "X-RapidAPI-Key": "748ddc26b8mshb590d128ea3a157p115cd4jsn1816f6826646"},
-        method: "GET"
-    
-    }).then(function (response)
-    {
-        var results = response.list;
-        console.log(results);
-
-        for(var i = 0; i < results.length; i++)
-        {
-            $("#urbanPlace").html(results[i].definition);
-        }
+        var queryURL = queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=" + myExpression;
+       
+$.ajax({
+     url: queryURL,
+     method: "GET"
+   }).then(function(response) {
+   var results = response.data.image_original_url;
+   console.log(response);
+    var gif = "<img src='"+results+"'>";
+  // var dictionaryGif = $("<img>");
+    $("#gifDiv").html(gif);
     })
-})
+    })
 })
